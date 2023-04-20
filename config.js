cc4d7e3feec96762fd81ffcd6d18ef337c0ba425
hwimli-mouheb <mouheb.houimli@gmail.com>
2023-04-20 15:29:10 +0100

    linked firestore

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxa-TFIc4Rb8wS6YM4NFdNv3Z2q2iIyoM",
-  authDomain: "smart-door-430d3.firebaseapp.com",
-  projectId: "smart-door-430d3",
-  storageBucket: "smart-door-430d3.appspot.com",
-  messagingSenderId: "830861055097",
-  appId: "1:830861055097:web:0200670a7a72232dc8f0a8",
-  measurementId: "G-T5WJGC6HNZ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
