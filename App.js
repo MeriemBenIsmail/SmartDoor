a27f8f1dce96990181d09ac4a94876f55ecd3d74
hwimli-mouheb <mouheb.houimli@gmail.com>
2023-04-20 15:27:19 +0100

    linked firestore

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import HomeScreen from "./screens/HomeScreen";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import { firebase } from './config';
+import { authentication, db } from "./config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { collection, getDoc } from "firebase/firestore";
 
 
 const App = () => {
@@ -13,25 +19,44 @@ const App = () => {
   const handleNavigate = (nextScreen) => {
     setScreen(nextScreen);
   };
+  const registerUser = async (email ,password, fullname) => {
+    try {
+      createUserWithEmailAndPassword(authentication, email, password).then((res) => {
+        setDoc(doc(db, "Users", authentication.currentUser.uid), {
+          fullname,
+          email,
+          });
+      })
+    }catch (error) {
+      alert(error.message)
+    }
+  }
   const loginUser = async (email ,password) => {
     try {
-      await firebase.auth().signInWithEmailAndPassword(email, password)
+      signInWithEmailAndPassword(authentication, email, password).then(async (res) => {
+        const uid = authentication.currentUser.uid;
+        const docRef = await doc(db, "Users", uid);
+        const docSnap = await getDoc(docRef);
+        // log user additional info for the moment 
+        console.log(docSnap.data())
+      })
     } catch (error) {
       alert(error.message)
     }
   }
-  const registerUser = async (email ,password) => {
-    try {
-      await firebase.auth().createUserWithEmailAndPassword(email, password).then(
-        (userCredential) => {
-          // Signed in 
-          var user = userCredential.user;
-        }
-      )
-    } catch (error) {
-      alert(error.message)
-    }
+  const getCurrentUserInfo = () => {
+    const userID = authentication.currentUser.uid;
+    const docRef = doc(db, "Users", userID);
+    getDoc(docRef).then((docSnap) => {
+      if (docSnap.exists()) {
+        //log user additional info for the moment
+        console.log(docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    });
   }
+  
   const renderScreen = () => {
     switch (screen) {
       case "home":
@@ -46,6 +71,7 @@ const App = () => {
           <SignInScreen
             handleNavigate={handleNavigate}
             onNavigate={() => handleNavigate("home")}
+            signIn={loginUser}
           />
         );
       case "signup":
@@ -53,6 +79,7 @@ const App = () => {
           <SignUpScreen
             handleNavigate={handleNavigate}
             onNavigate={() => handleNavigate("signup")}
+            signUp={registerUser}
           />
         );
         case "welcome":
